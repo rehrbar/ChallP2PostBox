0fb444d854a3d2093af8048b5cb5ef6a4d53640f
Roman Ehrbar <r1ehrbar@hsr.ch>
2016-05-22 19:53:51 +0200

    Added command to unlock device.

--- TapBoxWebApplication/TapBoxWebjob/Program.cs
+++ TapBoxWebApplication/TapBoxWebjob/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using Microsoft.Azure;
+using TapBoxCommon;
 
 namespace TapBoxWebjob
 {
@@ -9,11 +10,17 @@ namespace TapBoxWebjob
         private static void Main()
         {
             var client = new ReceiverClient(CloudConfigurationManager.GetSetting("AzureIoTHub.ConnectionString"));
+            var command = new Commands(CloudConfigurationManager.GetSetting("AzureIoTHub.ConnectionString"));
 
             client.OnNewMessageEvent += (id, uid) =>
             {
                 Console.WriteLine($"Device: {id} - UID: {uid}");
                 // TODO check against database and send c2d message if it's allowed.
+                if (uid == "112233")
+                {
+                    Console.WriteLine("Lucky guess? - Device shall be unlocked, your Majesty!");
+                    command.SendUnlockAsync(id);
+                }
             };
 
             client.ReceiveMessages();
