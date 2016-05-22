fb5c3f42091045509d4c7f57e8fbac2c238664ff
Roman Ehrbar <r1ehrbar@hsr.ch>
2016-05-22 14:58:25 +0200

    Added implementation to use EventHubProcessor. Is not that intuitive to use and some configuration needs to be hardcoded.

---
+++ TapBoxWebApplication/TapBoxCommon/Commands.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+namespace TapBoxCommon {
+    public class Commands
+    {
+        private readonly ServiceClient _serviceClient;
+
+        public Commands(string connectionString)
+        {
+            _serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+        }
+
+        /// <summary>
+        ///     Sends a c2d message through IoT Hub to the device.
+        /// </summary>
+        /// <param name="identity">Device id of the receiver.</param>
+        public async Task SendOpenLockAsync(string identity) {
+            var commandMessage = new Message(Encoding.ASCII.GetBytes("OpenLock"));
+            await _serviceClient.SendAsync(identity, commandMessage);
+        }
+    }
+}
--- TapBoxWebApplication/TapBoxWebjob/Program.cs
+++ TapBoxWebApplication/TapBoxWebjob/Program.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
 
 namespace TapBoxWebjob {
     // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
     class Program {
         // Please set the following connection strings in app.config for this WebJob to run:
         // AzureWebJobsDashboard and AzureWebJobsStorage
-        static void Main() {
-            var host = new JobHost();
+        static void Main()
+        {
+            var config = new JobHostConfiguration();
+            var eventHubConfig = new EventHubConfiguration();
+
+            eventHubConfig.AddReceiver(Functions.EventHubName, CloudConfigurationManager.GetSetting("AzureIotHub.EventHubEndpoint"));
+            config.UseEventHub(eventHubConfig);
+
+            var host = new JobHost(config);
             // The following code ensures that the WebJob will be running continuously
             host.RunAndBlock();
         }
